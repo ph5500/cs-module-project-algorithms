dfe69371007f6da9c5b602115ee106c8517eabe6
ph5500 <philfives@gmail.com>
2020-06-17 15:26:35 -0600

    passing tests for cookies and zeroes

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,30 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
 
-    pass
+
+def eating_cookies(n, cache=0):
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
+    elif cache and cache[n] > 0:
+        return cache[n]
+    else:
+        if not cache:
+            cache = [0 for _ in range(n+1)]
+        cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    return cache[n]
+
+# first pass
+# def eating_cookies(n, current):
+#     if current > n:
+#         return 0
+#     if current == n:
+#         return 1
+#     return eating_cookies(n, current+1) + eating_cookies(n, current+2) + eating_cookies(n, current+3)
+
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -5,8 +5,28 @@ Returns: a List of integers
 def moving_zeroes(arr):
     # Your code here
 
-    pass
-
+    zeroArr = []
+    i = 0
+    
+    while i < len(arr):
+        # if the value of the current index is 0, add it to zeroArr and remove
+        # it from the current array
+        
+        # the pointer isn't moved in case the ollowing index
+        # which becomes the current index is also a zero
+        if arr[i] == 0:
+            zeroArr.append(arr[i])
+            arr.remove(arr[i])
+        # if it isn't zero, the point
+        else:
+            i += 1
+            
+    # once loop above is finished, every element from the zeroArr is appended
+    # to the current array
+    for i in zeroArr:
+        arr.append(i)
+        
+    return arr
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
